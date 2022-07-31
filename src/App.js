db5415ebf4b4af9d4c052920f3e634b2f00b9780
irlfslab <climeastro@gmail.com>
2022-07-31 01:04:06 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import FormValidator from './FormValidator'
+
+function App () {
+  return (
+    <div className='App'>
+      <FormValidator />
+    </div>
+  )
+}
+
+export default App
